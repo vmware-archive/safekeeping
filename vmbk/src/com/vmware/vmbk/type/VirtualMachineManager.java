03dac28fcb3e29c3178726ca85140667bc359790
mdaneri <mdaneri@vmware.com>
2019-09-18 16:53:22 -0700

    Update VirtualMachineManager.java
    fix a compile issue

--- vmbk/src/com/vmware/vmbk/type/VirtualMachineManager.java
+++ vmbk/src/com/vmware/vmbk/type/VirtualMachineManager.java
@@ -43,6 +43,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.xml.ws.BindingProvider;
